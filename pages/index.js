3c5a4d4754a9a385513f7c2cfcf8841cab584493
Toan Phan <toanphan.cs@gmail.com>
2022-03-12 23:55:53 +0100

    feat: update homepage (#1)
    * change home page
    
    * update
    
    * escape character
    
    * fix
    
    * update

--- pages/index.js
+++ pages/index.js
@@ -5,49 +5,38 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Toan&apos;s Playground</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Read{' '}
-          <Link href="/wordle">
-            <a>wordle</a>
-          </Link>
+          Hi!
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to my little playground. Please feel free to look around.
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://playground.toanphan.dev/wordle" className="card">
+            <h3>Wordle Solver &rarr;</h3>
+            <p>Want to get those 3/6 on the trending game Wordle?</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="" className="card">
+            <h3>TBD?? &rarr;</h3>
+            <p></p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className="card">
+            <h3>TBD?? &rarr;</h3>
+            <p></p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="" className="card">
+            <h3>TBD?? &rarr;</h3>
+            <p></p>
           </a>
         </div>
       </main>
